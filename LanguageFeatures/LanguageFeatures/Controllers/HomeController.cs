aa5077f0844ebbcf1df48bb31b5b6061caea7dea
KimHyeonJoong <hj_kim@payletter.com>
2019-01-08 20:59:29 +0900

    first commit

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigatge to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            // 새로운 product 개체를 생성한다.
+            Product myProduct = new Product();
+
+            // 속성 값을 설정한다.
+            myProduct.Name = "Kayak";
+
+            // 속성 값을 가져온다.
+            string productName = myProduct.Name;
+
+            // 뷰를 생성한다.
+            return View("Result", (object)String.Format("Product name: {0}", productName));
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        private string name;
+
+        public string Name{ get; set; }
+    }
+}
